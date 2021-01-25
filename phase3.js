f36b610baf682ee7af7df36f58525aaf66d3ea00
michael azulay <michazulay97@gmail.com>
2021-01-25 15:51:17 +0200

    js3

--- phase3.js
+++ phase3.js
@@ -1,4 +1,15 @@
-function backTo2(event){
+
+
+
+document.querySelector('#finish').addEventListener('click', function(event){
     event.preventDefault();
-    window.location.href = 'phase-2.html';
-}
+    let hobbiesInput = document.querySelector('#hobiselect').Value,
+        imgInput = document.querySelector('#imgInput').Value;
+
+    localStorage.setItem("hobbies", hobbiesInput);
+    localStorage.setItem("id's image", imgInput);
+
+        alert('thank you');
+    document.getElementById('summary').classList.remove('.hidden');
+});
+

58db663a81ba883e177d7857a0997657ab6a1ec9
Nitaytalker <nitay68428@gmail.com>
2021-01-25 14:54:36 +0200

    3---------- phase2.js

--- phase-2.js
+++ phase-2.js
@@ -4,7 +4,8 @@ const inputs = document.querySelectorAll('input');
 function nextButton(event) {
     event.preventDefault();
 
-    // need get input of phase 1 and check them 
+    // need get input of phase 1 and check them \
+    
 
     let number = Number(inputs[2].value);
     console.log(number)
@@ -20,6 +21,9 @@ function nextButton(event) {
     }
 
     // need save inputs in local storeg
+    localStorage.setItem("city", inputs[0].value);
+    localStorage.setItem("street", inputs[1].value);
+    localStorage.setItem("Addressnumber", number);
 
     // go to phase3.html..
     window.location.href = 'phase3.html';
@@ -29,3 +33,26 @@ function backButton(event){
     event.preventDefault();
     window.location.href = 'phase1.html';
 }
+
+function inStart(){
+    let emailCheck =localStorage.getItem("name");
+    let birthInput =localStorage.getItem("email");
+    let nameInput =localStorage.getItem("birthday");
+    console.log(emailCheck);
+    if(emailCheck==null || emailCheck == false){
+        alert('Wrong email address.');
+        window.location.href = 'phase1.html';
+        return;
+    }
+    if(birthInput==null || birthInput == ''){
+        alert('Enter Birthday');
+        window.location.href = 'phase1.html';
+        return;
+    }
+    if (nameInput==null || nameInput.length < 2) {
+        alert("Name must have at least 3 letters!");
+        window.location.href = 'phase1.html';
+        return;
+    }
+}
+inStart();
